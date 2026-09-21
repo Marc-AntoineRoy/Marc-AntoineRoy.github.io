b8c83d6ff7221d08dca583dfbee5be9e34ba6658
Marc-A. R. <marcantoine.roy03@gmail.com>
2026-09-21 15:53:30 -0400

    imports js?

--- src/scripts/ComponentFactory.js
+++ src/scripts/ComponentFactory.js
@@ -1,7 +1,13 @@
+import Header from './components/Header.js';
+import Scrolly from './components/Scrolly.js';
+
 export default class ComponentFactory {
   constructor() {
     this.componentInstances = [];
-    this.componentList = {};
+    this.componentList = {
+      Header,
+      Scrolly,
+    };
     this.init();
   }
   init() {
